939d61446b4b09609b2b59e0dca193df64d964bc
nourddin4 <76815899+nourddin4@users.noreply.github.com>
2021-04-08 09:22:34 +0100

    sprint 3, js workshop

--- app.js
+++ app.js
@@ -5,14 +5,35 @@ document.getElementById("value").innerText=x;
 function decrement(){
   x=x-1;
   document.getElementById("value").innerText=x;
+
+  if(x < 0){
+    document.getElementById("value").style= "color:red;";
+  }
+
+  if(x==0){
+      document.getElementById("value").style= "color:back;";
+  }
+
 }
 
 function reset(){
     x=0;
     document.getElementById("value").innerText=x;
-  }
+    document.getElementById("value").style= "color:black;";
+}
 
 function increment(){
   x=x+1;
   document.getElementById("value").innerText=x;
+
+  if(x > 0){
+    document.getElementById("value").style= "color:green;";
+  }
+
+
+  if(x==0){
+    document.getElementById("value").style= "color:back;";
+  }
+
+
 }
